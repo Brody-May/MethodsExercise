f463bf0151b96cd26d9a033417118b964a86d1c2
Brody May <brodymay2019@outlook.com>
2023-01-31 15:35:48 -0500

    exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your quest?");
+            string quest = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is the airspeed volocity of an unladen swallow?");
+            string velocity = Console.ReadLine();
+
         }
     }
 }
