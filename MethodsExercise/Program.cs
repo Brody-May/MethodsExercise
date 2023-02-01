270b32f23e60743e6de1209ddfef6592ba3f1ac0
Brody May <brodymay2019@outlook.com>
2023-02-01 10:41:50 -0500

    Method changes

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,18 +1,28 @@
-﻿namespace MethodsExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("What is your quest?");
-            string quest = Console.ReadLine();
-            Console.WriteLine("What is your favorite color?");
-            string color = Console.ReadLine();
-            Console.WriteLine("What is the airspeed volocity of an unladen swallow?");
-            string velocity = Console.ReadLine();
+﻿namespace MethodsExercise;
 
-        }
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your quest?");
+        string quest = Console.ReadLine();
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine("What is the airspeed volocity of an unladen swallow?");
+        string velocity = Console.ReadLine();
+        Console.WriteLine($"Hello {name}");
+        Console.WriteLine($"Your quest is {quest}");
+        Console.WriteLine($"Your favoirite color is {color}");
+        Console.WriteLine($"Your guess is, {velocity}");
+
+        Console.WriteLine(Methods.Add(5, 8));
+        Console.WriteLine(Methods.Multiply(5, 8));
     }
+    
+
+   
+    
 }
